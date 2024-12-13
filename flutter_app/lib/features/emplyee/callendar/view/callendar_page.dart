88cc00e46587ece51c81408a5b4ebf3df349af38
0leslaw <119787710+0leslaw@users.noreply.github.com>
2024-12-13 15:25:47 +0100

    calendar initialised

--- flutter_app/lib/features/emplyee/callendar/view/callendar_page.dart
+++ flutter_app/lib/features/emplyee/callendar/view/callendar_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CallendarPage extends StatelessWidget {
   const CallendarPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TableCalendar(
+      // firstDay: DateTime.utc(2023, 1, 1),
+      // lastDay: DateTime.utc(2024, 12, 31),
+      // focusedDay: DateTime.now(),
+      
+    );
   }
 }
